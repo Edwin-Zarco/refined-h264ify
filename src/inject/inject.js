e64edbd76cb9dd5bfd24a18a1a66cc18c793e470
Edwin-Zarco <64220994+Edwin-Zarco@users.noreply.github.com>
2023-04-18 03:07:06 -0500

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -23,50 +23,49 @@
  * SOFTWARE.
  */
 
-function inject () {
-
+function inject() {
   override();
 
   function override() {
     // Override video element canPlayType() function
-    var videoElem = document.createElement('video');
-    var origCanPlayType = videoElem.canPlayType.bind(videoElem);
-    videoElem.__proto__.canPlayType = makeModifiedTypeChecker(origCanPlayType);
+    const videoElem = document.createElement('video');
+    const origCanPlayType = videoElem.canPlayType.bind(videoElem);
+    videoElem.canPlayType = makeModifiedTypeChecker(origCanPlayType);
 
     // Override media source extension isTypeSupported() function
-    var mse = window.MediaSource;
+    const mse = window.MediaSource;
     // Check for MSE support before use
-    if (mse === undefined) return;
-    var origIsTypeSupported = mse.isTypeSupported.bind(mse);
+    if (!mse) return;
+    const origIsTypeSupported = mse.isTypeSupported.bind(mse);
     mse.isTypeSupported = makeModifiedTypeChecker(origIsTypeSupported);
   }
 
-  // return a custom MIME type checker that can defer to the original function
+  // Return a custom MIME type checker that can defer to the original function
   function makeModifiedTypeChecker(origChecker) {
     // Check if a video type is allowed
-    return function (type) {
-      if (type === undefined) return '';
-      var disallowed_types = [];
-      if (localStorage['enhanced-h264ify-block_h264'] === 'true') {
+    return function(type) {
+      if (!type) return '';
+      const disallowed_types = [];
+      if (localStorage.getItem('refined-h264ify-block_h264') === 'true') {
         disallowed_types.push('avc');
       }
-      if (localStorage['enhanced-h264ify-block_vp8'] === 'true') {
+      if (localStorage.getItem('refined-h264ify-block_vp8') === 'true') {
         disallowed_types.push('vp8');
       }
-      if (localStorage['enhanced-h264ify-block_vp9'] === 'true') {
+      if (localStorage.getItem('refined-h264ify-block_vp9') === 'true') {
         disallowed_types.push('vp9', 'vp09');
       }
-      if (localStorage['enhanced-h264ify-block_av1'] === 'true') {
+      if (localStorage.getItem('refined-h264ify-block_av1') === 'true') {
         disallowed_types.push('av01');
       }
 
       // If video type is in disallowed_types, say we don't support them
-      for (var i = 0; i < disallowed_types.length; i++) {
-        if (type.indexOf(disallowed_types[i]) !== -1) return '';
+      for (const disallowedType of disallowed_types) {
+        if (type.indexOf(disallowedType) !== -1) return '';
       }
 
-      if (localStorage['enhanced-h264ify-block_60fps'] === 'true') {
-        var match = /framerate=(\d+)/.exec(type);
+      if (localStorage.getItem('refined-h264ify-block_60fps') === 'true') {
+        const match = /framerate=(\d+)/.exec(type);
         if (match && match[1] > 30) return '';
       }
 
@@ -77,28 +76,27 @@ function inject () {
 }
 
 function useActualVolumeLevel() {
-
-  if (localStorage['enhanced-h264ify-disable_LN'] !== 'true') {
+  if (localStorage.getItem('refined-h264ify-disable_LN') !== 'true') {
     return;
   }
 
-  var video = document.querySelector('video');
-  var config = {attributes: true};
+  const video = document.querySelector('video');
+  const config = { attributes: true };
 
-  var onVolumeChange = function(mutationList) {
-    var attr = 'aria-valuenow';
-    for (var mutation of mutationList) {
-      if (mutation.attributeName == attr) {
+  const onVolumeChange = function(mutationList) {
+    const attr = 'aria-valuenow';
+    for (const mutation of mutationList) {
+      if (mutation.attributeName === attr) {
         // Get current volume level from player's attribute
         // and set the actual volume
         video.volume = mutation.target.attributes[attr].value / 100;
       }
     }
-  }
+  };
 
-  var volumePanel = document.querySelector('.ytp-volume-panel');
+  const volumePanel = document.querySelector('.ytp-volume-panel');
   if (volumePanel) {
-    var observer = new MutationObserver(onVolumeChange);
+    const observer = new MutationObserver(onVolumeChange);
     observer.observe(volumePanel, config);
   }
 }
